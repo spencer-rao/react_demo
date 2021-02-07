1a14022316c93880135e5551ffa95140a5c4bc75
zhangyumei <81049477@yonghui.cn>
2021-02-07 11:32:42 +0800

    用Hook改写游戏模块

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,105 +2,13 @@
  * @Description:  
  * @Author: zhang yumei
  * @Date: 2021-02-03 10:24:45
- * @LastEditTime: 2021-02-05 20:33:46
+ * @LastEditTime: 2021-02-07 11:11:18
  * @LastEditors: zhang yumei
  */
-import React from "react"
+import React, { useState } from "react"
 import "./Game.less"
 import Board from "./component/Board.js"
-// import "./Game.css"
-class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            square: Array(9).fill(null),
-            route: true,
-            all: Array(9).fill(null),
-            last: 1,
-            total: 0,
-        }
-    }
-    handleClick(i) {
-        this.setState({ total: this.state.total + 1 })
-        this.setState({ route: !this.state.route })
-        let all = this.state.all.slice()
-        const squares = this.state.square.slice();
-        if (this.state.route) { squares[i] = "X" } else { squares[i] = "O" }
-        this.setState({ square: squares })
-        all = all.concat(squares)
-        this.setState({ all: all })
-    }
-    friststep() {
-        let all = this.state.all.slice()
-        let total = this.state.total
-        let last = this.state.last
-        this.setState({ last: this.state.last + 1 })
-        console.log(last, "s")
-        console.log(total, "s")
-        if (last < (total + 1)) {
-            this.setState({ route: !this.state.route });
-            this.setState({ square: all.slice(all.length - 9 * (last + 1), all.length - 9 * last) })
-        } else {
-            alert("非法操作")
-            return
-        }
-    }
-    laststep() {
-        let last = this.state.last
-        let all = this.state.all.slice()
-        let total = this.state.total
-        console.log(last)
-        console.log(total)
-        if (0 < last && last <= (total + 1)) {
-            this.setState({ last: this.state.last - 1 })
-            this.setState({ route: !this.state.route });
-            this.setState({ square: all.slice(all.length - 9 * (last - 1), all.length - 9 * (last - 2)) })
-        } else {
-            alert("非法操作")
-            return
-        }
-
-
-    }
-    render() {
-        const winner = calculateWinner(this.state.square);
-        if (winner === "X") {
-            setTimeout(() => {
-                alert("X获胜")
-                this.setState({ square: Array(9).fill(null) })
-                this.setState({ all: Array(9).fill(null) })
-                this.setState({ last: [] })
-            }, 0);
-        } else if (winner === "O") {
-            setTimeout(() => {
-                alert("O获胜")
-                this.setState({ square: Array(9).fill(null) })
-                this.setState({ all: Array(9).fill(null) })
-                this.setState({ last: [] })
-            }, 0)
-        }
-        return (
-            <div className="game" >
-                <div className="game-board">
-                    <Board
-                        square={this.state.square}
-                        route={this.state.route}
-                        onClick={(i) => this.handleClick(i)}
-                        status={this.state.status} />
-                </div>
-                <div className="game-info">
-                    <div><button
-                        onClick={() => this.friststep()}>显示上一步</button></div>
-                    <div><button
-                        onClick={() => this.laststep()}>显示下一步</button></div>
-                </div>
-            </div>
-        );
-    }
-}
-
-// ========================================
-function calculateWinner(squares) {
+function calculateWinner(square) {
     const lines = [
         [0, 1, 2],
         [3, 4, 5],
@@ -113,11 +21,86 @@ function calculateWinner(squares) {
     ];
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            console.log(squares[a], "8888");
-            return squares[a];
+        if (square[a] && square[a] === square[b] && square[a] === square[c]) {
+            console.log(square[a], "8888");
+            return square[a];
         }
     }
     return null;
+};
+function Game(props) {
+    const [square, setsquare] = useState(Array(9).fill(null));
+    const [route, setroute] = useState(true);
+    const [all, setall] = useState(Array(9).fill(null));
+    const [last, setlast] = useState(1);
+    const [total, settotal] = useState(0);
+    const winner = calculateWinner(square);
+
+    const handleClick = (i) => {
+        settotal(total + 1);
+        setroute(!route);
+        const squares = square.slice();
+        if (route) { squares[i] = "X" } else { squares[i] = "O" };
+        setsquare(squares);
+        setall(all.concat(squares));
+    };
+    const friststep = () => {
+        setlast(last + 1);
+        console.log(last, "s");
+        console.log(total, "s");
+        if (last < (total + 1)) {
+            setroute(!route);
+            setsquare(all.slice(all.length - 9 * (last + 1), all.length - 9 * last));
+        } else {
+            alert("非法操作");
+            return;
+        }
+    };
+    const laststep = () => {
+        console.log(last)
+        console.log(total)
+        if (0 < last && last <= (total + 1)) {
+            setlast(last - 1)
+            setroute(!route);
+            setsquare(all.slice(all.length - 9 * (last - 1), all.length - 9 * (last - 2)))
+        } else {
+            alert("非法操作")
+            return
+        }
+    };
+    if (winner === "X") {
+        setTimeout(() => {
+            alert("X获胜")
+            setsquare(Array(9).fill(null))
+            setall(Array(9).fill(null))
+            setlast([])
+        }, 0);
+    } else if (winner === "O") {
+        setTimeout(() => {
+            alert("O获胜")
+            setsquare(Array(9).fill(null))
+            setall(Array(9).fill(null))
+            setlast([])
+        }, 0)
+    };
+    return (
+        <div className="game" >
+            <div className="game-board">
+                <Board
+                    square={square}
+                    route={route}
+                    onClick={(i) => handleClick(i)}
+                />
+            </div>
+            <div className="game-info">
+                <div><button
+                    onClick={() => friststep()}>显示上一步</button></div>
+                <div><button
+                    onClick={() => laststep()}>显示下一步</button></div>
+            </div>
+        </div>
+    );
 }
+
+
 export default Game
